cd57efa33fa1e8754a45a122beb0fff2941be4b0
Dezzyboy <dezzyboy@tradebit.ca>
2022-11-06 18:05:55 -0500

    v2

---
+++ contracts/interfaces/IJioERC20.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity 0.6.12;
+
+interface IJioERC20 {    
+    function initialize(
+        string memory _name, string memory _symbol, uint8 _decimals, address owner) external;
+
+    function mint(address to, uint256 amount) external;
+}
